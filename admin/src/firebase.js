3c85d1685f40c22ac662b115026d2721fcca3deb
Olayiwola Adepoju <78272392+layiwola008@users.noreply.github.com>
2022-08-21 01:35:37 +0100

    Update firebase.js
    add your firebase api

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUEPDIP1p4Xy7kL4XNQXWqBcaaT1XbxPU",
-  authDomain: "netflix-fa053.firebaseapp.com",
-  projectId: "netflix-fa053",
-  storageBucket: "netflix-fa053.appspot.com",
-  messagingSenderId: "1017254801688",
-  appId: "1:1017254801688:web:d0a1b46f77479cb109ee6d"
+ // add your firebase api
 };
 
 const app = initializeApp(firebaseConfig);
